c05057da5d438b92f73a1b564755f802d87e359d
trihuelam <s3938804@rmit.edu.vn>
2024-01-08 13:02:23 +0700

    add video

--- assets/js/script.js
+++ assets/js/script.js
@@ -275,6 +275,31 @@ Version      : 1.0
 		});
 	}
 	
+
+	$(document).ready(function () {
+		$('.tutor-video .tutor-vid').on('click', function () {
+			// get required DOM Elements
+			var iframe_src = $(this).children('iframe').attr('src'),
+				iframe = $('.tutor-video-popup'),
+				iframe_video = $('.tutor-video-popup iframe'),
+				close_btn = $('.close-video');
+			iframe_src = iframe_src + '?autoplay=1&rel=0'; // for autoplaying the popup video
+
+			// change the video source with the clicked one
+			$(iframe_video).attr('src', iframe_src);
+			$(iframe).fadeIn().addClass('show-video');
+
+			// remove the video overlay when clicking outside the video
+			$(document).on('click', function (e) {
+				if ($(iframe).is(e.target) || $(close_btn).is(e.target)) {
+					$(iframe).removeClass('show-video');
+					$(iframe_video).attr('src', '');
+				}
+			});
+
+		});
+
+	});
 	// Chat
 
 	var chatAppTarget = $('.chat-window');
